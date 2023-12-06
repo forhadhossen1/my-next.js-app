218ba46556214bdcee48e6769931c94e49b674cb
forhadhossen1 <mr.forhad1030@gamil.com>
2023-12-06 13:37:27 +0600

    setup next js

---
+++ src/app/contact/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h3>Welcome to ContactPage </h3>
+        </div>
+    );
+};
+
+export default ContactPage;
